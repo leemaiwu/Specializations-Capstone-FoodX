aeb4fc508466184868404dec173ab4dd6a52a0b0
Laura Wu <leemaiwu@gmail.com>
2023-05-31 14:40:38 -0600

    Installed more packages

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,1 +1,14 @@
-const {DATATYPES} = require('sequelize')
+const {DataTypes} = require('sequelize')
+const {sequelize} = require('../util/database')
+
+module.exports = {
+    Ingredients: sequelize.define('ingredients', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        ingredients: DataTypes.STRING
+    })
+}
