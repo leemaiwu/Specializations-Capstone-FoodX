5049c90403743ca9b8ceffec62e31d0a5dda0a75
Laura Wu <leemaiwu@gmail.com>
2023-06-03 16:55:31 -0600

    Added cors to backend

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: 'https://foodx.onrender.com' }));
 
 require('dotenv').config()
 const PORT = process.env.PORT || 8000
