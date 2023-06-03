dc93feee77e3651b7c6c4a8f3e32a7a4b8f81728
Laura Wu <leemaiwu@gmail.com>
2023-06-03 17:49:30 -0600

    Added cors headers in response for deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@
 
 // app.use(express.json())
 // // app.use(cors())
-// app.use(cors({ origin: 'https://foodx.onrender.com' }));
+// app.use(cors({ origin: 'https://foodx.onrender.com' }))
 
 // require('dotenv').config()
 // const PORT = process.env.PORT || 8000
@@ -48,6 +48,12 @@ const PORT = process.env.PORT || 8000
 const { sendIngredients } = require('./controllers/ingredients')
 
 const { sequelize } = require('./util/database')
+
+// Add the following middleware before your API routes
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://foodx.onrender.com')
+  next()
+})
 
 app.post('/completions', sendIngredients)
 
