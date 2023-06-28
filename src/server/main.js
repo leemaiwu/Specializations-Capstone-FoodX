61234da5f17f8987b45dd7eddb4596cfd7ff8ffd
Laura Wu <leemaiwu@gmail.com>
2023-06-28 12:42:08 -0600

    Updated cors orgin on server end

--- src/server/main.js
+++ src/server/main.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(cors({ origin: 'https://foodx-server-ccgv.onrender.com' }))
+app.use(cors({ origin: 'https://foodx.onrender.com' }))
 
 require('dotenv').config()
 const PORT = 3005
