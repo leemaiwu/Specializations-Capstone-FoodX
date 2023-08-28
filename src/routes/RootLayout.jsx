5260fe927b756bbb5b75e7e1aad97258c51cef17
Laura Wu <leemaiwu@gmail.com>
2023-08-28 10:24:53 -0600

    AI prompt update and restructured

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -18,11 +18,24 @@ function RootLayout() {
     const options = {
       method: 'POST',
       body: JSON.stringify({
-        message: `As a skilled chef with knowledge of delicious cuisines from around the world, I need your expertise to create a mouthwatering recipe using the following ingredients: ${ingredientInput}. Please keep in mind that I am looking for a recipe that beautifully combines these ingredients while ensuring a delightful taste experience, please do not add any extra ingredients beyond what I listed.
+        // message: `As a skilled chef with knowledge of delicious cuisines from around the world, I need your expertise to create a mouthwatering recipe using the following ingredients: ${ingredientInput}. Please keep in mind that I am looking for a recipe that beautifully combines these ingredients while ensuring a delightful taste experience, please do not add any extra ingredients beyond what I listed.
 
-        It is essential that the recipe incorporates the provided ingredients and any spices or seasonings to bring out their flavors. I encourage you to explore various cooking methods and techniques that will enhance the taste and texture of the final dish.
+        // It is essential that the recipe incorporates the provided ingredients and any spices or seasonings to bring out their flavors. I encourage you to explore various cooking methods and techniques that will enhance the taste and texture of the final dish.
         
-        Furthermore, please include the recipe title, relevant cooking and preparation times, serving sizes, and step-by-step directions. Thank you!`,
+        // Furthermore, please include the recipe title, relevant cooking and preparation times, serving sizes, and step-by-step directions. Thank you!`,
+        message: `As a skilled chef with knowledge of delicious cuisines from around the world, I need your expertise to create a mouthwatering recipe using real and edible ingredients. Please ensure that all the ingredients I provide are suitable for human consumption. If any of the provided ingredients are not edible or real, please prompt me to enter valid edible ingredients.
+
+        Once I provide the edible ingredients, I'm looking for a recipe that beautifully combines these ingredients while ensuring a delightful taste experience. Please do not add any extra ingredients beyond what I list.
+        
+        It is essential that the recipe incorporates only the provided edible ingredients and any spices or seasonings to bring out their flavors. I encourage you to explore various cooking methods and techniques to enhance the taste and texture of the final dish.
+        
+        To ensure the safety and enjoyment of all, please note that any provided ingredients must be suitable for human consumption.
+        
+        Finally, please include the recipe title, relevant cooking and preparation times, serving sizes, and step-by-step directions.
+        
+        Here are the ingredients: ${ingredientInput}
+        
+        Thank you!`,
         ingredients: ingredientInput
       }),
       headers: {
